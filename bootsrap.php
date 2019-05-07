4ef788c5cbbeb706bbd67fe9eefce17b5fb36a6e
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2019-05-07 13:43:34 +0200

    Trouble with empty

--- bootsrap.php
+++ bootsrap.php
@@ -2,27 +2,43 @@
 // bootstrap
 // use backend to hide secret word
 
+session_start();
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	var_dump($_POST);
+	// var_dump($_SERVER);
+}
+
+//var_dump($_GET);
+
 // if get or post
-var_dump($_GET);
+//var_dump($_GET);
 // array(1) { ["page"]=> string(5) "again" } 
 
-//if ($_GET == ["page"] || $_GET == ["start"]){
-	
-//}
+if ($_GET == ["page"] || $_GET == ["start"]){
+	// CLEAR WORD
+	$_SESSION['word'] = null;
+	//@header("Content-type: text/html; charset=utf-8");
+	//header("/index.php");
+	//header('Refresh: 1; url=index.php');
+	//exit();
+}
+
+
 
 // use session to store word and ghessed char
 // store failed attemts
 
-session_start();
 
-// CLEAR WORD
-// $_SESSION['word'] = null;
+
+
 
 if(empty($_SESSION['word'])){
 	$words = file('wordlist.10000.txt', FILE_IGNORE_NEW_LINES);
 	$_SESSION['word'] = $words[rand(0, sizeof($words))];
 }
-var_dump($_SESSION);
+//var_dump($_SESSION);
 
 // get random word
 $word = $words[rand(0, sizeof($words))];
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('bootsrap.php');?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
@@ -12,13 +13,26 @@
 	<header><h1>Welcome to Hangman</h1>Ghess the word!<br/></header>
 	<article>
 <?php
-include('bootsrap.php');
+
 echo $word;
 echo strlen($word)
 ?>
 		<br />
 
-		<a href="again">try again</a>
+<form method="post" action="/">
+	<input type="text" value="test"></input>
+	<input type="email"></input>
+	<input type="submit" value="try_again"/>
+</form>
+
+<form action="index.php" method="post" enctype="multipart/form-data">
+	<p>Please select the file you would like to upload.
+	<input type="file" name="upload"> 
+	<br>
+	<input type="submit" value="Upload File">
+</form> 
+
+<!--		<a href="again">try again</a>-->
 	</article>
 	<footer></footer>
 </body>
