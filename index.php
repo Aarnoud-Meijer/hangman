ddd1544a51a9a634c14236c5340150cc7455b10c
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2019-05-07 11:45:13 +0200

    use backend to hide secret word

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 // bootstrap
+// use backend to hide secret word
+
 
 $words = file('wordlist.10000.txt', FILE_IGNORE_NEW_LINES);
 
-var_dump($words);
+//var_dump($words);
+// echo sizeof($words);
+// rand ( int $min , int $max ) : int
+// echo rand(0, sizeof($words));
+
+echo $words[rand(0, sizeof($words))];
+
